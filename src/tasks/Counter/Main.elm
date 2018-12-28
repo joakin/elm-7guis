faf7ab14c652826e4fcc1b4df74dbf2e671e29fb
joakin <joaquin@chimeces.com>
2018-12-28 15:05:23 +0100

    Task 1: Counter

--- src/tasks/Counter/Main.elm
+++ src/tasks/Counter/Main.elm
@@ -1,10 +1,21 @@
 module Main exposing (main)
 
-import Html exposing (div, p, text)
+import Browser
+import Html exposing (button, div, input, text)
+import Html.Attributes exposing (readonly, type_, value)
+import Html.Events exposing (onClick)
 
 
 main =
+    Browser.sandbox
+        { init = 0
+        , update = \_ count -> count + 1
+        , view = view
+        }
+
+
+view count =
     div []
-        (List.range 0 100
-            |> List.map (always (p [] [ text "test" ]))
-        )
+        [ input [ type_ "text", value (String.fromInt count), readonly True ] []
+        , button [ onClick () ] [ text "Count" ]
+        ]
