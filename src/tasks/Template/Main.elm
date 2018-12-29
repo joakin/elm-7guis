4c4c679c67d5f652b6ee88f65f60c7ec7b4c09d7
joakin <joaquin@chimeces.com>
2018-12-29 16:51:35 +0100

    Fix Template to be more helpful

--- src/tasks/Template/Main.elm
+++ src/tasks/Template/Main.elm
@@ -1,10 +1,43 @@
 module Main exposing (main)
 
-import Html exposing (div, p, text)
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 
 
+type Model
+    = Model
+
+
+type Msg
+    = Msg
+
+
+main : Program () Model Msg
 main =
+    Browser.sandbox
+        { init = init
+        , update = update
+        , view = view
+        }
+
+
+init : Model
+init =
+    Model
+
+
+update : Msg -> Model -> Model
+update msg model =
+    model
+
+
+view : Model -> Html Msg
+view model =
     div []
-        (List.range 0 100
-            |> List.map (always (p [] [ text "test" ]))
-        )
+        []
+
+
+box attrs children =
+    div (style "padding" "0.5rem" :: attrs) children
